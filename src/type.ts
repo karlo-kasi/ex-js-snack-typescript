f36972e482cd9956ff2b2c8c722ed8d3c5cab489
karlo-kasi <karlokasi1995@gmail.com>
2025-05-23 16:31:04 +0200

    Snack 3

--- src/type.ts
+++ src/type.ts
@@ -6,4 +6,17 @@ type Dipendente = {
     anniDiServizio: number[]
     readonly emailAziendale: string
     contratto: "indeterminato" | "deterdminato" | "freelance"
+
+}
+
+type Developer = Dipendente & {
+    livelloEsperienza: "Junior" | "Mind" | "Senior",
+    linguaggi: string[],
+    certificazioni: []
+}
+
+type ProjectManager = Developer & {
+    teamSize: number | null,
+    budgetGestito?: number
+    stakeholderPrincipali?: string[]
 }
